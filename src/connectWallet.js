c1ac11a18c875287c042280b5367be52d9941e1e
deffiedefi <76966041+deffiedefi@users.noreply.github.com>
2021-03-11 12:59:28 -0800

    3js & assets
    added assets for threejs & started building out landing

---
+++ src/connectWallet.js
@@ -0,0 +1,10 @@
+import Web3 from 'web3';
+
+const connectWallet = () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      window.ethereum.enable();
+      return true;
+    }
+    return false;
+  }
