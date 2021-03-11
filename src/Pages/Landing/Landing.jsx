99411af51c1e62e1901a1ebf7ab281ceec1dbf17
deffiedefi <76966041+deffiedefi@users.noreply.github.com>
2021-03-11 12:59:28 -0800

    3js & assets
    added assets for threejs & started building out landing

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -8,10 +8,19 @@ import NavBar from '../../Components/NavBar/NavBar.jsx'
 const Landing = () => {
 	const { activeData, activeDataHandler } = useContext(StateContext);
     return (
-        <div>
-			<div>Regal</div>
-			<NavBar/>
-		</div>
+        <div class="bounds">
+			<div class="text-box">
+      <div class="heading">The future of Nfts.</div>
+      <div class="button-wrapper">
+          <div class="button">Drop</div>
+          <div class="button" onclick='metamaskCall()'>Connect Wallet</div>
+          <script src = 'connectWallet.js'></script>
+      </div>
+    </div>
+    <script src="three.min.js"></script>
+	<script src="src\threebg.js"></script>
+	<NavBar/>
+	</div>
     )
 }
 
