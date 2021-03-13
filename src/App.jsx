1fcc0b9d1be4f2fc7e0308acca5c84f2bd5c8912
Ramtin Khoee <ramtin.khoee@gmail.com>
2021-03-12 21:43:29 -0800

    added marketplace page with metamask integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Route, Switch, useLocation, useHistory } from "react-router-dom";
 import Landing from './Pages/Landing/Landing.jsx';
 import Marketplace from './Pages/Marketplace/Marketplace.jsx';
-import { AnimatePresence} from "framer-motion";
+
 
 
 
 const App = () => {
       return (
-        <AnimatePresence >
-        <Switch location={location} key={location.pathname}>        
+
+        <Switch>        
+            {/* <Route exact path='/' component={Landing}/> */}
             <Route exact path='/' component={Landing}/>
             <Route exact path='/marketplace' component={Marketplace}/>
         </Switch>
-        </AnimatePresence >
+
 )}
 
 export default App;
