26bee919fb6e697913b8dab36f802654dfa735b2
deffiedefi <76966041+deffiedefi@users.noreply.github.com>
2021-03-12 17:58:46 -0800

    formatted mktplce
    added some content to marketplace, need logo and content to display.

--- src/Pages/Marketplace/Marketplace.jsx
+++ src/Pages/Marketplace/Marketplace.jsx
@@ -2,17 +2,24 @@ import React, { useState, useEffect } from 'react';
 import './Marketplace.css';
 import { StateContext } from '../../Providers/StateProvider.jsx';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react'
-import NavBar from '../../Components/NavBar/NavBar.jsx'
+import { useContext } from 'react';
+import NavBarFooter from '../../Components/Footer/Footer.jsx';
 
 const Marketplace = () => {
 	const { activeData, activeDataHandler } = useContext(StateContext);
     return (
-        <div>
-        <div>Marketplace Page</div>
-        <NavBar></NavBar>
+        <div className='marketplace-body'>
+            <ul>
+                <div className='logo'>Logo</div>
+                <div className='marketplace-btn'>Marketplace</div>
+                <div className='apply-btn'>Apply</div>
+            </ul>
+        <div className='scrollcontent'>
+                <div className='feature'>FEATURE.mp4</div>
+                <div className='headline'>featured artist: deffie</div>
         </div>
-
+        {/* <Footer></Footer> */}
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx'
 import StateProvider from './Providers/StateProvider.jsx';
 import { BrowserRouter } from 'react-router-dom'
- 
+import './index.css'
 //import our Landing, that is linked to the root and 
 ReactDOM.render(
   <BrowserRouter>
   <StateProvider>
       <App />
   </StateProvider>
-  </BrowserRouter> ,
+  </BrowserRouter>,
   document.getElementById('root')
 );
