57187cc0c46d36d26c2df31f054fe039c7e6786a
Ramtin Khoee <ramtin.khoee@gmail.com>
2021-03-14 00:39:42 -0800

    added profile page, reworked web3 logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Route, Switch, useLocation, useHistory } from "react-router-dom";
 import Landing from './Pages/Landing/Landing.jsx';
-import Marketplace from './Pages/Marketplace/Marketplace.jsx';
+import Explore from './Pages/Explore/Explore.jsx';
+import Profile from './Pages/Profile/Profile.jsx';
+import NavBar from './Components/NavBar/NavBar.jsx'
 
 
 
 
 const App = () => {
-      return (
+      return (      
 
-        <Switch>        
-            {/* <Route exact path='/' component={Landing}/> */}
+        <Switch>
             <Route exact path='/' component={Landing}/>
-            <Route exact path='/marketplace' component={Marketplace}/>
+            <Route exact path='/profile' component={Profile}/>
+            <Route exact path='/explore' component={Explore}/>
         </Switch>
 
 )}
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,21 +6,63 @@ import { useContext } from 'react'
 import profile from '../../../assets/profile.png'
 
 const NavBar = () => {
-    const { activeData, activeDataHandler } = useContext(StateContext);
-    let acc = window.ethereum.selectedAddress
-    // if (!window.ethereum.selectedAddress) {
-    //     acc = '';
-    // }
-    if (acc.length > 1) {
-        acc = acc.split('').slice(0, 4)
-        acc.toString()
+    const { userAddressHandler } = useContext(StateContext);
+    const [web3Auth, setWeb3Auth] = useState(false)
+    let accounts;
+    let url = window.location.pathname
+
+
+    const connectWallet = async () => { 
+        accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        userAddressHandler(accounts[0])
+        setWeb3Auth(!web3Auth);
     }
-    return (
+
+    let acc = window.ethereum.selectedAddress
+    let displayAcc = displayAcc || ''
+    if (acc == null) {
+        displayAcc = <button className='button-landing-mm-nav' onClick={connectWallet}>connect wallet</button>
+    } else if (window.ethereum.networkVersion !== '1') {
+        displayAcc = 'test network detected!'
+        displayAcc = <div className='button-landing-test'>{displayAcc}</div>
+
+    }   
+    else {
+        let accStart = acc.split('').slice(0, 4).join('').toString()
+        let accEnd = acc.split('').slice(38, 42).join('').toString()
+        displayAcc = accStart + '...' + accEnd
+        displayAcc =<div className='button-landing-acc-nav'>{displayAcc}</div>
+    }
+
+    
+    if (url.includes('explore')) {
+        return (
+            <div className='navbar'>
+                <Link to='/explore'><button className='current'>explore</button></Link>
+                <Link to='/marketplace'><button className='button-nav'>marketplace</button></Link>
+                <Link to='/apply'><button className='button-nav'>apply</button></Link>
+                <div className='profile'><div className='profile-nav-button'>{displayAcc}<Link className='profile' to='/profile'><img className='profile-nav'src={profile} width='55px'/></Link></div></div>
+            </div>)
+    } else if (url.includes('marketplace')){
+        return (<div className='navbar'>
+        <Link to='/explore'><button className='button-nav'>explore</button></Link>
+        <Link to='/marketplace'><button className='current'>marketplace</button></Link>
+        <Link to='/apply'><button className='button-nav'>apply</button></Link>
+        <div className='profile'><div className='profile-nav-button'>{displayAcc}<Link className='profile' to='/profile'><img className='profile-nav'src={profile} width='55px'/></Link></div></div>
+    </div>)
+    } else if (url.includes('apply')){
+        return (<div className='navbar'>
+        <Link to='/explore'><button className='button-nav'>explore</button></Link>
+        <Link to='/marketplace'><button className='button-nav'>marketplace</button></Link>
+        <Link to='/apply'><button className='current'>apply</button></Link>
+        <div className='profile'><div className='profile-nav-button'>{displayAcc}<Link className='profile' to='/profile'><img className='profile-nav'src={profile} width='55px'/></Link></div></div>
+    </div>)
+    } else return (
         <div className='navbar'>
+            <Link to='/explore'><button className='button-nav'>explore</button></Link>
             <Link to='/marketplace'><button className='button-nav'>marketplace</button></Link>
             <Link to='/apply'><button className='button-nav'>apply</button></Link>
-            <Link to='/'><button className='button-nav'>home</button></Link>
-            <div className='profile'><button className='profile-nav-button'><p>{acc}</p><img className='profile-nav'src={profile} width='50px'/></button></div>
+            <div className='profile'><div className='profile-nav-button'>{displayAcc}<Link className='profile' to='/profile'><img className='profile-nav'src={profile} width='55px'/></Link></div></div>
         </div>
     )
 }
