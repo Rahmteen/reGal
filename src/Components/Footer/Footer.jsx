a931ec1d038d769a5956d16534d7d2c995b76cff
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-03-30 09:38:45 -0700

    template of website

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+// import of basic context state
 import { StateContext } from '../../Providers/StateProvider.jsx';
+// import of local styling
 import './Footer.css';
+// link for future routing
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
     const { activeData, activeDataHandler } = useContext(StateContext);
+    // empty module for future use
     return (
         <div>Footer</div>
     )
