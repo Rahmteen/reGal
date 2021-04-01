05c6a5964dfcf5a1ce56c1d1afa0b10c175a8f8a
Jake Greer <Jakegreer11@gmail.com>
2021-04-01 11:56:21 -0700

    explore button changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@
 
 const path = require("path");
 const express = require("express");
-const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
 
 const env = require('dotenv').config()
