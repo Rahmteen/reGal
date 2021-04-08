8e3153e12a27fe70b4d799b311c3f83143ebeae3
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-04-08 15:23:54 -0700

    prep for merge

--- client/src/Minter/regalMinter_abi.js
+++ client/src/Minter/regalMinter_abi.js
@@ -1,13 +1,55 @@
 export const regalMinter = [
   {
-    "constant": false,
+    "constant": true,
     "inputs": [
       {
-        "name": "x",
+        "name": "interfaceId",
+        "type": "bytes4"
+      }
+    ],
+    "name": "supportsInterface",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "tokenId",
         "type": "uint256"
       }
     ],
-    "name": "set",
+    "name": "getApproved",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
     "outputs": [],
     "payable": false,
     "stateMutability": "nonpayable",
@@ -16,7 +58,7 @@ export const regalMinter = [
   {
     "constant": true,
     "inputs": [],
-    "name": "get",
+    "name": "totalSupply",
     "outputs": [
       {
         "name": "",
@@ -29,16 +71,414 @@ export const regalMinter = [
   },
   {
     "constant": true,
-    "inputs": [],
-    "name": "hello",
+    "inputs": [
+      {
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenOfOwnerByIndex",
     "outputs": [
       {
         "name": "",
-        "type": "string"
+        "type": "uint256"
       }
     ],
     "payable": false,
-    "stateMutability": "pure",
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenByIndex",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "ownerOf",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "setApprovalForAll",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "name": "_data",
+        "type": "bytes"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "name": "operator",
+        "type": "address"
+      }
+    ],
+    "name": "isApprovedForAll",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "photo",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "title",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "location",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "description",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "NFTUploaded",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "approved",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "ApprovalForAll",
+    "type": "event"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "photo",
+        "type": "string"
+      },
+      {
+        "name": "title",
+        "type": "string"
+      },
+      {
+        "name": "location",
+        "type": "string"
+      },
+      {
+        "name": "description",
+        "type": "string"
+      }
+    ],
+    "name": "uploadNFT",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "name": "to",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "name": "",
+        "type": "address"
+      },
+      {
+        "name": "",
+        "type": "address"
+      },
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getTotalNFTCount",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getNFT",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "name": "",
+        "type": "address[]"
+      },
+      {
+        "name": "",
+        "type": "string"
+      },
+      {
+        "name": "",
+        "type": "string"
+      },
+      {
+        "name": "",
+        "type": "string"
+      },
+      {
+        "name": "",
+        "type": "string"
+      },
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
     "type": "function"
   }
 ]
--- client/src/Pages/NftMinter/NftMinter.jsx
+++ client/src/Pages/NftMinter/NftMinter.jsx
@@ -1,65 +1,89 @@
 //Modules
-import React, { useEffect, useState } from 'react';
-import Web3 from 'web3';
-//Contracts
-import { regalMinter } from '../../Minter/regalMinter_abi';
+import React, { useEffect, useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import Web3 from "web3";
+import { regalMinter } from "../../Minter/regalMinter_abi";
 
-const web3 = new Web3(Web3.givenProvider|| "http://localhost:8080")
-const contractAddr = '0x08106e01F94402DF79199eE9fb339540a243C79B';
-const SimpleContract = new web3.eth.Contract(regalMinter, contractAddr);
+const web3 = new Web3(Web3.givenProvider || 'http://127.0.0.1:7545');
+const contractAddr = "0xaA0eC25450C61E09ba34da304B47AC2e23C7db30";
+const regalMinterContract = new web3.eth.Contract(regalMinter, contractAddr);
 
 const NftMinter = () => {
-    const [number, setNumber] = useState(0);
-    const [getNumber, setGetNumber] = useState('0x00');
+  const [number, setNumber] = useState(0);
+  const [ipfsUpload, setIpfsUpload] = useState('https://gateway.ipfs.io/ipfs/Qmau88PXLsH9PNjKRKNyGvmPiydbJv6Xff98LSoMckstoa');
+  const [location, setLocation] = useState('');
+  const [userAddress, setUserAddress] = useState("0x00");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    const accounts = window.ethereum.enable();
+  }, []);
+
+  const handleName = async (e) => {
+    e.preventDefault();
+    let result = e.target.value;
+    setName(result);
+    console.log(result);
+  };
+
+  // const handleName = async (e) => {
+  //   e.preventDefault();
+  //   setIpfsUpload(result);
+  // };
+
+  const handleMint = async (e) => {
+    e.preventDefault();
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    // const gas = await regalMinterContract.methods.uploadNFT(ipfsUpload, name, location, description).estimateGas();
+    // let result = await regalMinterContract.methods.uploadNFT(ipfsUpload, name, location, description).send({from: '0xF461A152De146B480cCE5B8fCbF6eC19DFC440A6'})
+    // console.log(result)
+    let result = await regalMinterContract.methods.getNFT(3).call()
+    console.log(result)
     
+  };
 
-    useEffect(() => {
-      console.log(SimpleContract.methods)
-    },[])
-
-    const handleGet = async (e) => {
-      e.preventDefault();
-      const result = await NFTMintContract.methods.get().call();
-      setGetNumber(result);
-      console.log(result);
-    }
-
-    const handleSet = async (e) => {
-      e.preventDefault();    
-      const accounts = await window.ethereum.enable();
-      const account = accounts[0];
-      const gas = await SimpleContract.methods.set(number).estimateGas();
-      const result = await SimpleContract.methods.set(number).send({
-        from: account,
-        gas 
-      })
-      console.log(result);
-    }
-
-    return (
-        <div className="App">
-          <header className="App-header">
-            <form onSubmit={handleSet}>
-              <label>
-                Set Number:
-                <input 
-                  type="text"
-                  name="name"
-                  value={number}
-                  onChange={ e => setNumber(e.target.value) } />
-              </label>
-              <input type="submit" value="Set Number" />
-            </form>
-            <br/>
-            <button
-              onClick={handleGet}
-              type="button" > 
-              Get Number 
-            </button>
-            { getNumber }
-          </header>
-        </div>  
-    );
-}
+  return (
+    <div className="form-flex">
+      <button className="ipfs-upload-button"></button>
+      <div className="mb-3 p-5">
+        <Form>
+          <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Label>NFT NAME</Form.Label>
+            <Form.Control type="textarea" placeholder="NFT NAME" onChange={(e) => handleName(e)}/>
+          </Form.Group>
+          <Form.Label>NFT PREVIEW FILE</Form.Label>
+          <Form.File id="formcheck-api-custom" custom>
+            <Form.File.Input isValid />
+            <Form.File.Label data-browse="Button text">
+              1000 x 1000
+            </Form.File.Label>
+            {/* <Form.Control.Feedback type="valid">
+              You did it!
+            </Form.Control.Feedback> */}
+          </Form.File>
+          <br/>
+          <br/>
+          <Form.Group controlId="exampleForm.ControlSelect2">
+            <Form.Label>LOCATION</Form.Label>
+            <Form.Control as="select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>DESCRIPTION</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+      </div>
+      <Button onClick={handleMint}>MINT</Button>
+    </div>
+  );
+};
 
 export default NftMinter;
