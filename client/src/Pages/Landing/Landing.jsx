2982f226ed4a6e8929a5c74a2a4dffe198d9ee08
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-04-11 22:03:24 -0700

    altered landing page particle.js

--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -2,18 +2,17 @@
 import React, { useState, useEffect } from "react";
 import Particles from "react-particles-js";
 import { Link } from "react-router-dom";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col } from "react-bootstrap";
 
 const Landing = () => {
   const [particleCount, setParticleCount] = useState(20);
 
   useEffect(() => {
-      //checks for small window and reduces particle count
-    if(window.matchMedia('screen and (max-width: 500px)').matches) 
-    {
+    //checks for small window and reduces particle count
+    if (window.matchMedia("screen and (max-width: 500px)").matches) {
       setParticleCount(10);
     }
-  },[])
+  }, []);
 
   // functionality to connect wallet on the user window when "logging into" the site.
   // const connectWallet = async () => {
@@ -22,130 +21,108 @@ const Landing = () => {
   //   return setWeb3Auth(!web3Auth);
   // };
 
-  return (  
+  return (
     <div id="landingPage">
-      <Particles 
+      <Particles
         params={{
-                particles: {
-                    number:{
-                        value: particleCount,
-                        density:{
-                            enable: false,
-                            value_area: 0
-                        }
-                    },
-                    color: {
-                        value:"#ffffff"
-                    },
-                    shape: {
-                        type: 'circle',
-                        stroke: { 
-                            width: 1,
-                            color: "#fff"
-                        },
-                        image: {
-                            src: "#",
-                            width: 400,
-                            height: 400
-                        }
-                    },
-                    opacity: {
-                        value: 0.8,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 1.0557003759917487,
-                            opacity_min: 0.1,
-                            sync: false
-                        }
-                    },
-                    size: {
-                        value: 3.008530152163807,
-                        random: true,
-                        anim: {
-                            enable: false,
-                            speed: 20,
-                            size_min: 2.1,
-                            sync: false
-                        }
-                    },
-                    line_linked: {
-                        enable: true,
-                        distance: 200,
-                        color: "#ffffff",
-                        opacity: 0.9,
-                        width: 2
-                    },
-                    move: {
-                        enable: true,
-                        speed: 1,
-                        direction: "none",
-                        random: false,
-                        straight: false,
-                        out_mode: "out",
-                        bounce: true,
-                        attract: {
-                            enable: false,
-                            rotateX: 600,
-                            rotateY: 1200
-                        }
-                    }
-                },
-                interactivity: {
-                    detect_on: "canvas",
-                    events: {
-                        onhover: {
-                            enable: true,
-                            mode:"grab"
-                        },
-                        onclick: {
-                            enable: true,
-                            mode:"push"
-                        },
-                        resize: true
-                    },
-                    modes: {
-                        grab: {
-                            distance: 400,
-                            line_linked: {
-                                opacity: 1
-                            }
-                        },
-                        bubble: {
-                            distance: 400,
-                            size: 20,
-                            duration: 2,
-                            opacity:8,
-                            speed: 3
-                        },
-                        repulse: {
-                            distance: 125,
-                            duration: 0.4
-                        },
-                        push: {
-                            particles_nb: 4
-                        },
-                        remove: { 
-                            particles_nb: 2
-                        }
-                    }
-                },
-                retina_detect: true
-            }}
-        />
-        <div className="landing-block mx-auto text-center">
+          particles: {
+            number: {
+              value: 250,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: "#ffffff",
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 1,
+                color: "#ffffff",
+              },
+              image: {
+                src: "#",
+                width: 0,
+                height: 0,
+              },
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: { enable: true, speed: 1, opacity_min: 0, sync: false },
+            },
+            size: {
+              value: 1,
+              random: true,
+              anim: { enable: false, speed: 1, size_min: 0.3, sync: false },
+            },
+            line_linked: {
+              enable: false,
+              distance: 150,
+              color: "#ffffff",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: .1,
+              direction: "bottom",
+              random: true,
+              straight: false,
+              out_mode: "out",
+              bounce: true,
+              attract: { enable: false, rotateX: 700, rotateY: 700 },
+            },
+          },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: { enable: true, mode: "repulse" },
+              onclick: { enable: true, mode: "grab" },
+              resize: true,
+            },
+            modes: {
+              repulse: {
+                distance: 400,
+                size: 1,
+                duration: 2,
+                opacity: 0,
+                speed: .5,
+              },
+              grab: { distance: 400 },
+              repulse: { distance: 20, duration: 0.4 },
+              push: { particles_nb: 4 },
+              remove: { particles_nb: 2 },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
+      <div className="landing-block mx-auto text-center">
         <Row>
-            <Col md={12}>
-                <h1 className="text-white text-majesti">Regal</h1>
-            </Col>
-            <Col md={12} className="mb-3">
-                <Link className="btn btn-light text-majesti enableEthereumButton" to="">Connect</Link>
-            </Col>
-            <Col md={12}>
-                <Link className="btn btn-light text-majesti rainbow-btn" to="/explore" >Explore</Link>
-            </Col>
+          <Col md={12}>
+            <h1 className="text-white text-majesti">Regal</h1>
+          </Col>
+          <Col md={12} className="mb-3">
+            <Link
+              className="btn btn-light text-majesti enableEthereumButton"
+              to=""
+            >
+              Connect
+            </Link>
+          </Col>
+          <Col md={12}>
+            <Link
+              className="btn btn-light text-majesti rainbow-btn"
+              to="/explore"
+            >
+              Explore
+            </Link>
+          </Col>
         </Row>
-        </div>
+      </div>
     </div>
   );
 };
