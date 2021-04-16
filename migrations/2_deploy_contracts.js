4df1987cefe2ebfcb6f0c74893694e75c5e811f3
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-04-16 16:22:01 -0700

    Redirect apply link to a new tab
    Co-authored-by: Danny <thestinx@gmail.com>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
 // Fetch the Storage contract data from the Storage.json file
 var RegalMinter = artifacts.require("./contracts/RegalMinter.sol");
-var AuctionHouse = artifacts.require("./contracts/AuctionHouse.sol");
-var RegalAuction = artifacts.require("./contracts/RegalAuction.sol");
-
+// var AuctionHouse = artifacts.require("./contracts/AuctionHouse.sol");
+// var RegalAuction = artifacts.require("./contracts/RegalAuction.sol");
+var RegalToken = artifacts.require("./contracts/RegalToken.sol")
+// var RegalNFT = artifacts.require("./contracts/RegalNFT.sol");
 // JavaScript export
 module.exports = function(deployer) {
     // Deployer is the Truffle wrapper for deploying
@@ -10,7 +11,8 @@ module.exports = function(deployer) {
 
     // Deploy the contract to the network
     deployer.deploy(RegalMinter);
-    deployer.deploy(AuctionHouse);
-    deployer.deploy(RegalAuction);
-
+    // deployer.deploy(AuctionHouse);
+    // deployer.deploy(RegalAuction);
+    deployer.deploy(RegalToken)
+    // deployer.deploy(RegalNFT)
 }
