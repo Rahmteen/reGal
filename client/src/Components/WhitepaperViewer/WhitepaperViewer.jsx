d2547e2000624d624e60509c4d7a5960ef59ed00
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-04-15 22:18:04 -0700

    added whitepaper and apply page + fixed routing

--- client/src/Components/WhitepaperViewer/WhitepaperViewer.jsx
+++ client/src/Components/WhitepaperViewer/WhitepaperViewer.jsx
@@ -1,32 +1,20 @@
-import React, { useState } from 'react';
-import RegalWhitepaper from '../../../assets/images/regal_whitepaper.pdf'
-import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Document, Page, pdfjs } from "react-pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import React from "react";
+import PDFViewer from 'pdf-viewer-reactjs'
+import 'bulma-helpers/css/bulma-helpers.min.css'
 
 const WhitepaperViewer = () => {
-    const [numPages, setNumPages] = useState(4);
-    const [pageNumber, setPageNumber] = useState(1);
-  
-    function onDocumentLoadSuccess({ numPages }) {
-      setNumPages(numPages);
-    }
-  
-    return (
-      <div>
-        <Container>
-        <Document
-          file={RegalWhitepaper}
-          onLoadSuccess={onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <p>Page {pageNumber} of {numPages}</p>
-        </Container>
-      </div>
-    );
+return (
+    <div className='pdf-nav'>
+    <PDFViewer
+      document={{
+        url: "../../../assets/images/regal_whitepaper.pdf",
+      }}
+      canvasCss='whitepaper-pdf'
+      showThumbnail={{ scale: 1 }}
+      hideZoom={true}
+    />
+    </div>
+  )
 };
 
 export default WhitepaperViewer;
