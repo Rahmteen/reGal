a00b272222402c93e09a4afa99c49c48c546f8df
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-04-16 18:38:14 -0700

    restyled footer and social icons
    Co-authored-by: Danny <thestinx@gmail.com>

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,69 +1,48 @@
 //Modules
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Footer = () => {
-    //const { activeData, activeDataHandler } = useContext(StateContext);
-    // empty module for future use - footer obviously
-    return (
-        <footer className="text-center text-white footer">
-            
-            <Container className="p-4 pb-0">
-                <section className="mb-4">
-                    <Button variant="outline-light floating" className="m-2"><i className="fab fw fa-twitter"></i></Button>
-                    <Button variant="outline-light floating" className="m-2"><i className="fab fw fa-instagram"></i></Button>
-                    <Button variant="outline-light floating" className="m-2"><i className="fab fw fa-discord"></i></Button>
-                    <Button variant="outline-light floating" className="m-2"><i className="fab fw fa-medium"></i></Button>
-                </section>
-
-                <section>
-                    <Form>
-                        <Row className="d-flex justify-content-center">
-                            <Col>
-                                <p className="pt-2">
-                                    <strong>Sign up for our newsletter</strong>
-                                </p>
-                            </Col>
-                            <Col md={5}>
-                                <div className="form-outline form-white mb-4">
-                                    <Form.Control type="email" placeholder="Email address" />
-                                </div>
-                            </Col>
-                            <Col>
-                                <button type="submit" className="btn btn-outline-light mb-4">Subscribe</button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </section>
-
-                <section className="mb-4">
-                    <p>
-                        We're creators who decided to build a platform for creators, without taking a massive percentage of the sales. Join the cause
-                        by submitting an application.
-                    </p>
-                </section>
-                    
-                <section>
-                    <Row>
-                        <Col lg={3} md={6} className="mb-4 mb-md-0">
-                            <h5 className="text-uppercase">FAQ</h5>
-                        </Col>
-                        <Col lg={3} md={6} className="mb-4 mb-md-0">
-                            <h5 className="text-uppercase">T.O.S.</h5>
-                        </Col>
-                        <Col lg={3} md={6} className="mb-4 mb-md-0">
-                            <h5 className="text-uppercase">Apply</h5>
-                        </Col>
-                        <Col lg={3} md={6} className="mb-4 mb-md-0">
-                            <h5 className="text-uppercase">Careers</h5>
-                        </Col>
-                    </Row>
-                </section>
-            </Container>
-        </footer>
-
-    )
-}
+  //const { activeData, activeDataHandler } = useContext(StateContext);
+  // empty module for future use - footer obviously
+  return (
+    <footer className="text-white footer">
+      <Container>
+          <Row className="footer-nav" >
+            <Col lg={2} md={3} sm={12}>
+              <h1 className="footer-links footer-logo text-majesti">Regal</h1>
+            </Col>
+            <Col lg={1} md={2} sm={12}>
+              <h5 className="footer-links text-uppercase">FAQ</h5>
+            </Col>
+            <Col lg={1} md={2} sm={12}>
+              <h5 className="footer-links text-uppercase">T.O.S.</h5>
+            </Col>
+            <Col lg={1} md={2} sm={12}>
+              <h5 className="footer-links text-uppercase">Apply</h5>
+            </Col>
+            <Col lg={1} md={2} sm={12}>
+              <h5 className="footer-links text-uppercase">Whitepaper</h5>
+            </Col>
+            </Row>
+            <div className="social-icons pt-2 mt-2">
+              <Button className="social-footer" variant="floating">
+                <i className="fab fw fa-twitter"></i>
+              </Button>
+              <Button className="social-footer" variant="floating">
+                <i className="fab fw fa-instagram"></i>
+              </Button>
+              <Button className="social-footer" variant="floating">
+                <i className="fab fw fa-discord"></i>
+              </Button>
+              <Button className="social-footer" variant="floating">
+                <i className="fab fw fa-medium"></i>
+              </Button>
+            </div>
+      </Container>
+    </footer>
+  );
+};
 
 export default Footer;
