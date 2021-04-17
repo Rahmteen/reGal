47e74f4e1326ae28dd064d5437b15a9d9f9eab3c
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-04-17 13:44:55 -0700

    added footer image, lightened text color in explore
    Co-authored-by: Danny <thestinx@gmail.com>

--- client/src/Pages/Explore/Explore.jsx
+++ client/src/Pages/Explore/Explore.jsx
@@ -15,6 +15,7 @@ import NftDisplay from "../../Components/NftDisplay/NftDisplay";
 //Media
 import sampleVid from "../../../assets/images/BMWEEER.gif";
 import cube from "../../../assets/images/Cubegif.gif";
+import footerImg from "../../../assets/images/b4footer.png";
 import demo from "../../../assets/images/demo-art.jpeg";
 import demo2 from "../../../assets/images/nft-1.jpg";
 import demo3 from "../../../assets/images/nft-2.jpg";
@@ -97,7 +98,7 @@ const Explore = () => {
             <p className="about-section text-white">
               Media on the internet is about to be flipped upside down. We're
               here to push the{" "}
-              <span className="text-primary">
+              <span style={{color: '#ed7779'}}>
                 decentralization of ownership in global media.
               </span>{" "}
             </p>
@@ -105,6 +106,9 @@ const Explore = () => {
         </Row>
         <Row>
           <Col className="h6 text-white mb-4" md={12}>
+            <div className="h1 text-white text-start text-majesti mb-n3">
+              Live Activity
+            </div>
             <div
               className="pb-2 text-end"
               style={{
@@ -116,7 +120,7 @@ const Explore = () => {
             </div>
           </Col>
         </Row>
-        <Row className="nft-display-rows">
+        <Row className="nft-display-rows pb-5">
           {nfts.length &&
             nfts.map((nft, index) => (
               <Col md={4} sm={6} key={index} className="mb-5">
@@ -130,13 +134,13 @@ const Explore = () => {
             ))}
         </Row>
         <Row>
-          <Col md={7} className="ml-auto mr-auto mt-5 mb-5 text-center">
+          <Col md={7} className="pb-5 ml-auto mr-auto mt-5 mb-5 text-center">
             <p className="text-white about-section">
               Regal was founded to{" "}
-              <span className="text-primary">disrupt the power imbalances</span>{" "}
+              <span style={{color: '#ed7779'}}>disrupt the power imbalances</span>{" "}
               that exist in the art industry today. We do not take a cut from
               your work, and{" "}
-              <span className="text-primary">we never will.</span>
+              <span style={{color: '#ed7779'}}>we never will.</span>
             </p>
           </Col>
         </Row>
@@ -151,6 +155,11 @@ const Explore = () => {
                 </p>
               </div>
             </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <Image className="b4-footer"src={footerImg} fluid></Image>
           </Col>
         </Row>
       </Container>
