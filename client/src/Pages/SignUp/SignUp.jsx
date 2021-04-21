c7cc7ed62ab55cbf00f43a1a8674771cee304cf1
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-04-20 17:50:30 -0700

    finished basic boilerplate for user signup page
    Co-authored-by: Danny <thestinx@gmail.com>

--- client/src/Pages/SignUp/SignUp.jsx
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 //Modules
 import React, { useEffect, useState } from "react";
-import { Container, Col, Row, Image, Form, Button } from "react-bootstrap";
+import { Container, Col, Row, Image, Form, Button, FormFile } from "react-bootstrap";
 import Circle from "../../../assets/images/crcle.gif";
 
 // username: string,
@@ -22,19 +22,26 @@ const SignUp = () => {
         </Col>
         <Col md={6} lg={6} sm={12}>
           <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
+          <Form.Group controlId="formWalletID">
+              <Form.Label className="text-white" >WalletID</Form.Label>
+              <Form.Control type="text" disable={true} placeholder="0xA760f51e900E5BaF1Be30836ab72373fA08BB906" />
+              <Form.Text className="text-white">
+                You're creating an account associated with this Wallet Address!
               </Form.Text>
             </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group controlId="formDisplayName">
+              <Form.Label className="text-white" >Display Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter display name" />
+              <Form.Text className="text-white">
+                The name that you will display to the world.
+              </Form.Text>
             </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+            <Form.Group controlId="formEmail">
+              <Form.Label className="text-white">Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-white">
+                We'll never share your email with anyone else.
+              </Form.Text>
             </Form.Group>
             <Button className="btn-regal" type="submit">
               Submit
