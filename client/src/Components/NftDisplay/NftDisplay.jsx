c9ea0e1c58408f9d4aae9e84a76770597ea96329
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-04-22 16:39:36 -0700

    resize nft card
    Co-authored-by: Danny <thestinx@gmail.com>

--- client/src/Components/NftDisplay/NftDisplay.jsx
+++ client/src/Components/NftDisplay/NftDisplay.jsx
@@ -19,7 +19,7 @@ const NftDisplay = ({
   return (
     // <Col fluid md={4} className="mb-5 nft-wrapper"></Col>
     <Fragment>
-      <Link className="card-link" >
+      <Link className="card-link">
         <Card className="nft-display mb-3">
           <Card.Img className="explore-card-image" src={image} />
           <Card.Body>
@@ -58,18 +58,36 @@ const NftDisplay = ({
               <Col className="nft-user">
                 {/* <img src={Profile} className="profile-image"/> */}
               </Col>
-
             </Row>
             <Row>
-            <Col lg={12} className="text-center price-nft">{current} Ξ</Col>
-            <Col lg={6} className="text-end mt-3 "><Link to={`/details/${id}`}><Button className="btn-regal ">Bid</Button></Link></Col>
-            <Col lg={6} className="text-start mt-3 d-flex align-items-center">
-                @{creator} <br/>
+              <Col
+                lg={12}
+                className="font-secondary text-center mt-3 text-uppercase text-primary font-italic"
+              >
                 {title}
-            </Col>
+              </Col>
+              <Col
+                lg={12}
+                className="offset-5 text-center mt-1 mb-4 mr-auto ml-auto"
+              >
+                @{creator}{" "}
+              </Col>
+
+              <Col lg={12} className="mt-1 d-flex align-items-center mb-3">
+                <Col className="text-end">
+                <Link to={`/details/${id}`}>
+                  <Button className="btn-regal">Bid</Button>
+                </Link>
+                </Col>
+                <Col className="price-col">{" "}current
+                <div className="price-nft ">Ξ {current}</div>{" "}
+                <div>${current * 2300}</div>
+                </Col>
+
+              </Col>
             </Row>
           </Card.Body>
-          <Card.Footer className="nft-footer">
+          <Card.Footer className="nft-footer mb-2">
             <div className="pr-3">
               <div className="fa fa-lg fa-heart-o pr-1"></div>
               {likes}
